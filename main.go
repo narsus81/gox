a3c64ca6b7e6efae6d5738a6f42104f2a86c24b0
Adminbck <info@develand.eu>
2024-08-24 13:19:47 +0200

    Release 0.0.18

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ type Config struct {
 }
 
 func InitGox() *Gox {
-	c := Config{port: 555, debug: true, version: "v0.0.18"}
+	c := Config{port: 444, debug: true, version: "v0.0.18"}
 	return &Gox{config: c}
 }
 
