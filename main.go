7a60adc910b3a2b24f6375aec85c9cec82965207
Adminbck <info@develand.eu>
2024-08-24 03:52:42 +0200

    versioning

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ type Gox struct {
 }
 
 type Config struct {
-	port  int
-	debug bool
+	port    int
+	debug   bool
+	version string
 }
 
 func InitGox() *Gox {
-	c := Config{port: 888, debug: true}
+	c := Config{port: 888, debug: true, version: "0.0.1"}
 	return &Gox{config: c}
+}
+
+func (g *Gox) Version() string {
+	return g.config.version
 }
